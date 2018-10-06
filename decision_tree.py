61fba06020510077f43e74e5785d118b1254d49e
Ryan Peterson <rtp4jc@gmail.com>
2018-10-06 16:48:52 -0500

    Add base decision tree

---
+++ decision_tree.py
@@ -0,0 +1,6 @@
+class DecisionTree:
+    def __init__(self,
+                 board,
+                 default_state_cap=1000):
+        self.board = board
+        self.default_state_cap = default_state_cap
